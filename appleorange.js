760b943082282424739766dbb57eecf22e9639ed
github_username <you@domain.com>
2022-08-02 03:09:03 -0700

    problem solving

---
+++ appleorange.js
@@ -0,0 +1,9 @@
+var moneyGiven = 1000;
+var apple = 400;
+var orange = 300;
+var expences = apple + orange;
+console.log(expences);
+var total = moneyGiven - expences;
+console.log(total);
+
+// problem 1 is solved this ans 300
---
+++ arry.js
@@ -0,0 +1,61 @@
+// target : display 0 to 9
+//  var rostEvent = 0;
+// while(rostEvent < 7 ){
+//     console.log('ami rost khai na')
+    // 3 symbol same just code write
+    // rostEvent =rostEvent +1;
+    // rostEvent += 1;
+//     console.log(rostEvent);
+//     rostEvent ++;
+// }
+
+// used in while loop
+
+// target display 1 to 10
+// var isNumber = 1;
+// while (isNumber <= 10){
+//     console.log('ami tomar kace jabo')
+//     isNumber ++;
+// } 
+
+// used in for loop
+
+// for loop & while loop simple just for written difrent
+// for(var isNumber = 1; isNumber<7; isNumber += 1){
+//     console.log('ami tomar var khabo na')
+// }
+
+// for lop different way
+
+//  for(i = 0; i<=7; i++){
+//     console.log(i);
+//  }
+
+// full Number pete jete ata jante hbe
+
+//  var isNumber = [32, 34, 45, 35, 64,];
+//  for (var i=0; i<7; i++){
+//     var isNumbers = isNumber[i];
+//     console.log(isNumber)
+//  }
+ 
+// var numbers = [ 45, 87, 89, 56, 32, 51, 188, 41, 25, 27];
+// for ( var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//      if( numbers > 50){
+//        continue;
+//     }
+//     console.log(number);
+// }
+
+// for(i=0; i<=30; i++){
+//     console.log(i);
+//     if(i>5){
+//         break;
+//     }
+// }
+
+
+for (var i = 10; i >= 1; i++){
+    console.log[i];
+}
