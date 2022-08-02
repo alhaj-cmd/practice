8c575f015bb7c601f030b189c1e692ed097e166f
github_username <you@domain.com>
2022-08-02 04:25:16 -0700

    problem is solved

---
+++ avarage.js
@@ -0,0 +1,12 @@
+// problem no 2s
+var Mathmatice =75.25;
+var Biology =65;
+var physics =80;
+var chemistry =35.45;
+var Bangla =99.50;
+var subject = Mathmatice+Biology+physics+chemistry+Bangla;
+console.log(subject);
+var totalmarks = subject / 5;
+// condition 3 towdecimal
+var towdecimal = totalmarks.toFixed(2);
+console.log(towdecimal);
---
+++ tovariable.js
@@ -0,0 +1,8 @@
+var tovari = ['apple', ' Banana', 'orange'] ;
+var toget = tovari.indexOf('Banana');
+tovari[toget] ='Mango'
+console.log(toget);
+tovari.pop();
+console.log(toget);
+tovari.push('watermalon');
+console.log(toget);
