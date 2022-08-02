29cc768d2f6db8047e045dc7af689b6af5cd024d
github_username <you@domain.com>
2022-08-02 11:08:31 -0700

    function commit

---
+++ consept.js
@@ -0,0 +1,5 @@
+var bookshop = 'hate colome javascript';
+var libray =[ 'javascript', 'english', 'economice', 'basic computer'];
+var librays = libray.indexOf('ami kicu janina');
+libray[1]='story';
+console.log(librays);
---
+++ func.js
@@ -0,0 +1,27 @@
+function addNum (num1, num2){
+    
+    var result = num1 * num2;
+    return result;
+}
+ var total= addNum(4, 40);
+ console.log('ami aci tomar', total);
+
+// function biyogkortaci
+
+ function biyogKoro (num1, num2){
+    console.log(num1, num2)
+    var result = num1 - num2;
+    return result;
+ }
+ var num1 = 123;
+ var num2 = 23;
+ var total = biyogKoro( num1, num2)
+ console.log(total);
+
+ // function math
+ function gunKoro (num1, num2){
+    var result = num1 - num2;
+    return result;
+ }
+ var total = gunKoro(234, 43);
+ console.log('ami gun korte aseci', total);
