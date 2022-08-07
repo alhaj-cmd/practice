d91ca142556ed04977d54f77f98b02aafcf58783
github_username <you@domain.com>
2022-08-07 12:24:33 -0700

    first commit

---
+++ object.js
@@ -0,0 +1,10 @@
+var students = {id : 22, name:'rohim', marks:34, phone:37448
+}
+console.log(students.marks);
+var objectInt = (students.name);
+console.log(objectInt);
+
+// difrent ways to object proparti
+
+students ['name']=ali;
+students.name = hosrot;
---
+++ string.js
@@ -0,0 +1,10 @@
+const isValid = 'title';
+const isnotValid = 'titlE';
+
+if (isValid.toLocaleLowerCase() === isnotValid.toLocaleLowerCase()){
+    console.log('incase is valid');
+    
+}
+else{
+    console.log('incase is not valid num')
+}
