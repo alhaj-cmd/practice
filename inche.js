30fbb8e8e3edd9a8436fd2b1855e2f3b7a62c265
github_username <you@domain.com>
2022-08-09 21:32:06 -0700

    problem solving

---
+++ inche.js
@@ -0,0 +1,12 @@
+ //const feet = 5;
+ function feetToInch(inches){
+    const inche = inches*12;
+    return inche;
+  
+//   //  return (parseInt(inches/12)+ ' ' + Math.floor(inches%12)+"'")
+ }
+ const fet = 5
+ const dadafet = feetToInch(fet);
+ console.log(dadafet);
+
+
